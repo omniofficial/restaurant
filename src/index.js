1fd4321212c72184573d63b2e4762410bc76728c
omniofficial <omniofficial04@gmail.com>
2026-02-04 21:51:05 -0800

    Pageload module added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import "./styles.css";
+import { pageLoad } from "./modules/pageload.js";
 console.log("Hello, World!");
+pageLoad();
---
+++ src/modules/pageload.js
@@ -0,0 +1,5 @@
+// Page load logic will go here. This will be the homepage.
+
+export function pageLoad() {
+    console.log("pageLoad function is running");
+}
