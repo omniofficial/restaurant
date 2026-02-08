10a191f10af949bd0a72069f7a9014e14416e347
omniofficial <omniofficial04@gmail.com>
2026-02-08 12:23:25 -0800

    Helper function

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -7,4 +7,25 @@ export function menu() {
 
     // Clear previous content
     content.textContent = "";
+
+    /* Generic container helper function */
+    function createGenericContainer(titleText, bodyText) {
+        const genericContainer = document.createElement("div");
+        genericContainer.classList.add("generic-container");
+
+        /* Inside of generic-container, assign what I passed in into divs */
+        const text = document.createElement("div");
+        text.classList.add("text");
+
+        const p = document.createElement("p");
+        p.textContent = titleText;
+
+        const h2 = document.createElement("h2");
+        h2.textContent = bodyText;
+
+        text.appendChild(h2);
+        text.appendChild(p);
+        genericContainer.appendChild(text);
+        return genericContainer;
+    }
 }
