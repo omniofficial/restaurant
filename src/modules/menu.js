c4fdfc9d703671f15673780dc1c43387ebbd9617
omniofficial <omniofficial04@gmail.com>
2026-03-02 18:14:44 -0800

    Uploaded images

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,5 +1,8 @@
 // MENU - Creates a div element, adds the appropriate content and styles to that element and then appends it to the DOM.
 
+import lollipopImage from "../images/lollipop.png";
+import 
+
 export function menu() {
     console.log("menu module is running");
     // Grab content div
@@ -28,5 +31,62 @@ export function menu() {
     contentContainer.classList.add("content-container");
 
     /* Generic container helper function (each menu item) */
-    function createMenuItem(title, description, price, image) {}
+    function createMenuItem(title, description, price, image) {
+        const menuItem = document.createElement("div");
+        menuItem.classList.add("menu-container");
+
+        /* Inside of each menu item, I want the following: */
+        const itemTitle = document.createElement("div");
+        itemTitle.textContent = title;
+        menuItem.appendChild(itemTitle);
+
+        const itemDescription = document.createElement("p");
+        itemDescription.textContent = description;
+        menuItem.appendChild(itemDescription);
+
+        const itemPrice = document.createElement("p");
+        itemPrice.textContent = price;
+        menuItem.appendChild(itemPrice);
+
+        const itemImage = document.createElement("img");
+        itemImage.src = image;
+        itemImage.alt = title;
+        menuItem.appendChild(itemImage);
+
+        return menuItem;
+    }
+
+    // Create test menu items */
+    contentContainer.appendChild(
+        createMenuItem(
+            "Lollipop",
+            "A savory lollipop for a savory treat",
+            "$2",
+            lollipopImage,
+        ),
+    );
+
+    contentContainer.appendChild(
+        createMenuItem(
+            "Tootsie Roll",
+            "The darkest of the darkest tootsie roll!",
+            "$1",
+            tootsierollImage,
+        ),
+    );
+
+    contentContainer.appendChild(
+        createMenuItem("Candy Cane", "A minty candy cane", "$1", candycaneImage),
+    );
+
+    contentContainer.appendChild(
+        createMenuItem(
+            "Candy Apple",
+            "A sweet and sour candy apple dipped in fudge",
+            "$5",
+            candyappleImage,
+        ),
+    );
+
+    content.appendChild(contentContainer);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ export default {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset/resource",
+            },
         ],
     },
 };
